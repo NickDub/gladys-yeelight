549a459fa939cef9d686ffb602493bd3730e13af
Dubois Nicolas <dubois.nicolas@free.fr>
2018-08-26 15:58:17 +0200

    add the command sending feature

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = function(sails) {
 	var setup = require('./lib/setup.js');
 	var init = require('./lib/init.js');
 	var exec = require('./lib/exec.js');
+	var send = require('./lib/send.js');
 
 	gladys.on('ready', function() {
 		init();
@@ -12,7 +13,8 @@ module.exports = function(sails) {
 		install : install,
 		setup : setup,
 		init : init,
-		exec : exec
+		exec : exec,
+		send : send
 	};
 
 };
